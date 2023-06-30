54a3e60f553e9199cf03043731ffec2275a50a06
Faheem6765 <codingalt7@gmail.com>
2023-06-30 20:45:42 +0500

    Changes cors issue

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const path = require("path");
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/Uploads',express.static('Uploads'));
 
@@ -24,18 +25,18 @@ const corsOptions = {
   // Linking Routes
   app.use(require("./Routes/ProjectsRoute"));
 
-  // app.get('/',(req,res) =>{
-  //   res.send('API is Running Successfully.')
-  // })
+  app.get('/',(req,res) =>{
+    res.send('API is Running Successfully.')
+  })
 
-  if (
-    process.env.NODE_ENV === "production" ||
-    process.env.NODE_ENV === "staging"
-  ) {
-    app.use(express.static(path.resolve(__dirname, "client", "build")));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-  }
+  // if (
+  //   process.env.NODE_ENV === "production" ||
+  //   process.env.NODE_ENV === "staging"
+  // ) {
+  //   app.use(express.static(path.resolve(__dirname, "client", "build")));
+  //   app.get("*", (req, res) => {
+  //     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  //   });
+  // }
   
   app.listen(PORT, () => {});
